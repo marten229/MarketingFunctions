60717b1c051f979b3ad7a57f53d605c4f53fa8a5
Marten Schulz <ms@marten-schulz.de>
2024-06-15 12:18:08 +0200

    Mehr Diagramme für die Analyse

--- views.py
+++ views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from .models import SpecialOffer, Event
-from .functions import update_restaurant_profile
 from ReviewFeedbackSystem.models import Bewertung
 from ReservationManagement.models import Reservation
-from RestaurantManagement.models import Restaurant
+from RestaurantManagement.models import Restaurant, MenuItem
 from UserManagement.models import User
 import matplotlib.pyplot as plt
 import io
@@ -27,7 +26,6 @@ def create_special_offer_view(request, restaurant_id):
             restaurant_id=restaurant_id, title=title, description=description,
             discount_rate=discount_rate, start_date=start_date, end_date=end_date,
             terms_conditions=terms_conditions)
-        update_restaurant_profile(restaurant_id)
     return render(request, 'create_special_offer.html')
 
 #@login_required
@@ -46,7 +44,6 @@ def create_event_view(request, restaurant_id):
         Event.objects.create(
             restaurant_id=restaurant_id, title=title, description=description,
             start_date=start_date, end_date=end_date, terms_conditions=terms_conditions)
-        update_restaurant_profile(restaurant_id)
     return render(request, 'create_event.html')
 
 #@login_required
@@ -62,29 +59,34 @@ def analyze_customer_data(request, pk):
     if form.is_valid():
         start_date = form.cleaned_data['start_date']
         end_date = form.cleaned_data['end_date']
-        
-        # Kundendaten sammeln
+
         reservations = Reservation.objects.filter(restaurant=restaurant, datum__range=[start_date, end_date])
         customer_ids = reservations.values_list('user_id', flat=True).distinct()
         customers = User.objects.filter(id__in=customer_ids, role='customer').distinct()
         
-        # Demografische Daten analysieren
         demographics = customers.values_list('birthdate', flat=True)
         ages = [(date.today() - birth_date).days // 365 for birth_date in demographics if birth_date]
 
-        # Buchungshistorie analysieren
-        booking_history = reservations.values('datum', 'restaurant_id')
+        booking_history = reservations.values('datum', 'uhrzeit', 'restaurant_id')
 
-        # Dining Preferences und Feedback analysieren
         reviews = Bewertung.objects.filter(user__in=customers, restaurant=restaurant, created_at__range=[start_date, end_date])
         dining_preferences = reviews.values('anlass')
         feedback = reviews.values('bewertung_gesamt', 'bewertung_service', 'bewertung_essen', 'bewertung_ambiente')
 
-        # Grafiken erstellen
-        fig, axes = plt.subplots(3, 3, figsize=(15, 10))
+        menu_items = MenuItem.objects.filter(restaurant=restaurant)
+        reservations_with_dishes = reservations.prefetch_related('gerichte')
+        
+        dish_counts = {}
+        for reservation in reservations_with_dishes:
+            for dish in reservation.gerichte.all():
+                if dish.name in dish_counts:
+                    dish_counts[dish.name] += 1
+                else:
+                    dish_counts[dish.name] = 1
+
+        fig, axes = plt.subplots(5, 3, figsize=(15, 18))
         fig.tight_layout(pad=4.0, w_pad=2.0, h_pad=10.0)
         
-        # Altersverteilung
         ax = axes[0, 0]
         if ages:
             ax.hist(ages, bins=10, color='skyblue', edgecolor='black')
@@ -92,7 +94,6 @@ def analyze_customer_data(request, pk):
         ax.set_xlabel('Alter')
         ax.set_ylabel('Anzahl an Kunden')
 
-        # Buchungshistorie nach Datum
         df_bookings = pd.DataFrame(list(booking_history))
         ax = axes[0, 1]
         if not df_bookings.empty:
@@ -103,17 +104,41 @@ def analyze_customer_data(request, pk):
         ax.set_xlabel('Tag')
         ax.set_ylabel('Anzahl der Reservierungen')
 
-        # Dining Preferences
-        df_preferences = pd.DataFrame(list(dining_preferences))
         ax = axes[0, 2]
+        if not df_bookings.empty:
+            df_bookings['monat'] = df_bookings['datum'].dt.to_period('M')
+            season_counts = df_bookings['monat'].value_counts().sort_index()
+            season_counts.plot(kind='bar', color='teal', ax=ax)
+        ax.set_title('Reservierungen nach Monat')
+        ax.set_xlabel('Monat')
+        ax.set_ylabel('Anzahl der Reservierungen')
+
+        ax = axes[1, 0]
+        if not df_bookings.empty:
+            df_bookings['uhrzeit'] = pd.to_datetime(df_bookings['uhrzeit'], format='%H:%M:%S').dt.hour
+            time_counts = df_bookings['uhrzeit'].value_counts().sort_index()
+            time_counts.plot(kind='bar', color='purple', ax=ax)
+        ax.set_title('Reservierungen nach Uhrzeit')
+        ax.set_xlabel('Uhrzeit')
+        ax.set_ylabel('Anzahl der Reservierungen')
+
+        df_preferences = pd.DataFrame(list(dining_preferences))
+        ax = axes[1, 1]
         if not df_preferences.empty:
             dining_counts = df_preferences['anlass'].value_counts()
             dining_counts.plot(kind='pie', autopct='%1.1f%%', ax=ax)
         ax.set_title('Essgewohnheiten')
 
-        # Feedback: Gesamtbewertung
+        ax = axes[1, 2]
+        if dish_counts:
+            dishes = pd.Series(dish_counts)
+            dishes.sort_values(ascending=False).plot(kind='bar', color='brown', ax=ax)
+        ax.set_title('Am meisten ausgewählte Gerichte')
+        ax.set_xlabel('Gericht')
+        ax.set_ylabel('Anzahl der Auswahl')
+
         df_feedback = pd.DataFrame(list(feedback))
-        ax = axes[1, 0]
+        ax = axes[2, 0]
         if not df_feedback.empty:
             gesamt_counts = df_feedback['bewertung_gesamt'].value_counts().sort_index()
             gesamt_counts.plot(kind='bar', color='green', ax=ax)
@@ -121,8 +146,7 @@ def analyze_customer_data(request, pk):
         ax.set_xlabel('Bewertung')
         ax.set_ylabel('Anzahl')
 
-        # Feedback: Service
-        ax = axes[1, 1]
+        ax = axes[2, 1]
         if not df_feedback.empty:
             service_counts = df_feedback['bewertung_service'].value_counts().sort_index()
             service_counts.plot(kind='bar', color='red', ax=ax)
@@ -130,8 +154,7 @@ def analyze_customer_data(request, pk):
         ax.set_xlabel('Bewertung')
         ax.set_ylabel('Anzahl')
 
-        # Feedback: Essen
-        ax = axes[1, 2]
+        ax = axes[2, 2]
         if not df_feedback.empty:
             essen_counts = df_feedback['bewertung_essen'].value_counts().sort_index()
             essen_counts.plot(kind='bar', color='blue', ax=ax)
@@ -139,8 +162,7 @@ def analyze_customer_data(request, pk):
         ax.set_xlabel('Bewertung')
         ax.set_ylabel('Anzahl')
 
-        # Feedback: Ambiente
-        ax = axes[2, 0]
+        ax = axes[3, 0]
         if not df_feedback.empty:
             ambiente_counts = df_feedback['bewertung_ambiente'].value_counts().sort_index()
             ambiente_counts.plot(kind='bar', color='yellow', ax=ax)
@@ -148,11 +170,12 @@ def analyze_customer_data(request, pk):
         ax.set_xlabel('Bewertung')
         ax.set_ylabel('Anzahl')
 
-        # Leere Plots entfernen
-        fig.delaxes(axes[2, 1])
-        fig.delaxes(axes[2, 2])
+        fig.delaxes(axes[3, 1])
+        fig.delaxes(axes[3, 2])
+        fig.delaxes(axes[4, 0])
+        fig.delaxes(axes[4, 1])
+        fig.delaxes(axes[4, 2])
 
-        # Grafiken in einem Puffer speichern
         buf = io.BytesIO()
         plt.savefig(buf, format='png')
         buf.seek(0)
