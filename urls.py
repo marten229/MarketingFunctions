d4f14619358976e2cbba96569355270b6f56feb3
Marten Schulz <ms@marten-schulz.de>
2024-06-17 21:12:11 +0200

    chore: Remove unused functions and update restaurant profile

--- urls.py
+++ urls.py
@@ -1,11 +1,5 @@
 from django.urls import path
-from .views import (
-    create_special_offer_view,
-    manage_special_offers_view,
-    create_event_view,
-    manage_events_view,
-    analyze_customer_data
-)
+
 
 urlpatterns = [
 
